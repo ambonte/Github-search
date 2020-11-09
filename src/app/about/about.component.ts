f825da37a1766c35aee09508bf0b6b19be6426e1
Bonte Emma BANA <bbonteemma@gmail.com>
2020-11-09 10:44:38 +0200

    adding changes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{ProfileService} from '../services/profile.service'
 
 @Component({
   selector: 'app-about',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  profile:any;
+  repos:any;
+   username:any;
 
-  constructor() { }
+  constructor(public profileService:ProfileService) {
+    this.profileService.getProfileInfo().subscribe(profile => {
+      console.log(profile);
+      this.profile=profile;
+    });
+    this.profileService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos=repos;
+    });
+  }
+  findProfile(){
+    this.profileService.updateProfile(this.username);
+    this.profileService.getProfileInfo().subscribe (profile => {
+      console.log(profile);
+      this.profile=profile;
+    });
+    this.profileService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos=repos;
+    });
+  }
 
-  ngOnInit(): void {
+  
+
+  ngOnInit() {
   }
 
 }
