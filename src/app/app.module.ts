d7b35fd506918b0f4b0573c71124d0cafd9d930e
Bonte Emma BANA <bbonteemma@gmail.com>
2020-11-09 11:03:48 +0200

    creating a pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
 import { ProfileComponent } from './profile/profile.component';
 import { GitComponent } from './git/git.component';
+import { DatePipe } from './date.pipe';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { GitComponent } from './git/git.component';
     NavbarComponent,
     AboutComponent,
     ProfileComponent,
-    GitComponent
+    GitComponent,
+    DatePipe
   ],
   imports: [
     BrowserModule,
