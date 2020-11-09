5329f978998c2a12d06b51619fc79427a6b05e48
Bonte Emma BANA <bbonteemma@gmail.com>
2020-11-09 10:54:38 +0200

    saving changes

--- src/app/git/git.component.spec.ts
+++ src/app/git/git.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GitComponent } from './git.component';
 
@@ -6,12 +6,12 @@ describe('GitComponent', () => {
   let component: GitComponent;
   let fixture: ComponentFixture<GitComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
       declarations: [ GitComponent ]
     })
     .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GitComponent);
