e117bc78b8d0fa605b4b661737107b0af4d805ba
Bonte Emma BANA <bbonteemma@gmail.com>
2020-11-09 10:51:46 +0200

    creating a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
 import { ProfileComponent } from './profile/profile.component';
+import { GitComponent } from './git/git.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     AboutComponent,
-    ProfileComponent
+    ProfileComponent,
+    GitComponent
   ],
   imports: [
     BrowserModule,
